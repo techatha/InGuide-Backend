e5f434037652a897ec9fa21a311a16d9f00d8519
techatha <techatham_p@cmu.ac.th>
2025-06-10 17:00:20 +0700

    Loaded model + add preprocess function

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from preprocess import preprocess
+import pickle
+import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
+
+try:
+    with open('Models/lightGBM-model_v1.pkl', 'rb') as f:
+        model = pickle.load(f)
+except FileNotFoundError:
+    print("Error: Model file 'lightGBM-model_v1.pkl' not found.")
+    model = None
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/predictMovement', methods=['POST'])
+def predictMovement():
+    if model is None:
+        return jsonify({"error": "Model not loaded. Please check the model file path."}), 500
+
+    try:
+        request_payload = request.get_json()
+        if request_payload is None:
+            return jsonify({"error": "Invalid JSON data provided."}), 400
+
+        if 'data' not in request_payload:
+            return jsonify({"error": "Missing 'data' array in JSON payload."}), 400
+
+        data_list = request_payload['data']
+        data_df = pd.DataFrame(data_list)
+        data_interval = request_payload.get('interval')
+        processed_data = preprocess(data_df, data_interval)
+
+        # Make prediction
+        prediction = model.predict(processed_data)
+
+        return jsonify({"prediction": prediction.tolist()}), 200  # Convert prediction to list for JSON
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
---
+++ preprocess.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+from scipy.fft import fft, fftfreq
+
+
+def rotate_accelerometer_to_world_frame(sensor_df):
+    acc_data = sensor_df[['acc_x', 'acc_y', 'acc_z']].copy()
+    acc_gravity = sensor_df[['acc_gx', 'acc_gy', 'acc_gz']].copy()
+    rotation_rate = sensor_df[['gyro_x', 'gyro_y', 'gyro_z']].copy()
+
+    columns = ['acc_x', 'acc_y', 'acc_z', 'acc_gx', 'acc_gy', 'acc_gz', 'mean_magnitude']
+    result_df = pd.DataFrame(columns=columns)
+    for acc, acc_g, gyro in zip(acc_data.itertuples(index=False), acc_gravity.itertuples(index=False),
+                                rotation_rate.itertuples(index=False)):
+        roll = gyro.gyro_x
+        pitch = gyro.gyro_y
+        yaw = gyro.gyro_z
+        r = R.from_euler('zyx', [yaw, pitch, roll], degrees=False)
+
+        acc_world = r.apply([acc.acc_x, acc.acc_y, acc.acc_z])
+        gravity_world = r.apply([acc_g.acc_gx, acc_g.acc_gy, acc_g.acc_gz])
+        mean_magnitude = np.linalg.norm(acc_world)
+
+        result = pd.DataFrame([list(acc_world) + list(gravity_world) + [mean_magnitude]], columns=columns)
+        result_df = pd.concat([result_df, result], ignore_index=True)
+
+    return result_df
+
+
+def compute_frequency_domain(signal, interval):
+    N = len(signal)
+    fs = 1000 / interval
+    frequencies = fftfreq(N, d=1 / fs)
+    fft_values = np.abs(fft(signal))
+
+    pos_mask = frequencies > 0
+    frequencies = frequencies[pos_mask]
+    fft_values = fft_values[pos_mask]
+
+    power = fft_values ** 2
+    return np.sum(frequencies * power) / np.sum(power), frequencies[np.argmax(fft_values)]
+
+
+def preprocess(data, data_interval=500):
+
+    features = [
+        'avg_acc_x', 'median_acc_x', 'std_acc_x', 'min_x', 'max_x', 'mean_abs_x',
+        'avg_acc_y', 'median_acc_y', 'std_acc_y', 'min_y', 'max_y', 'mean_abs_y',
+        'avg_acc_z', 'median_acc_z', 'std_acc_z', 'min_z', 'max_z', 'mean_abs_z',
+        'avg_acc_gx', 'avg_acc_gy', 'avg_acc_gz',
+        'mean_magnitude', 'signal_magnitude_area',
+        'mean_freq_x', 'dominant_freq_x',
+        'mean_freq_y', 'dominant_freq_y',
+        'mean_freq_z', 'dominant_freq_z',
+        'lat_diff', 'lon_diff'
+    ]
+    result_df = pd.DataFrame(columns=features)
+
+    rotated_df = rotate_accelerometer_to_world_frame(data)
+
+    # accelerometer of this window frame
+    mean_acc_x = rotated_df['acc_x'].mean()
+    median_acc_x = rotated_df['acc_x'].median()
+    std_acc_x = rotated_df['acc_x'].std()
+    min_acc_x = rotated_df['acc_x'].min()
+    max_acc_x = rotated_df['acc_x'].max()
+    mean_abs_x = rotated_df['acc_x'].abs().mean()
+    # ------------------------------------
+    mean_acc_y = rotated_df['acc_y'].mean()
+    median_acc_y = rotated_df['acc_y'].median()
+    std_acc_y = rotated_df['acc_y'].std()
+    min_acc_y = rotated_df['acc_y'].min()
+    max_acc_y = rotated_df['acc_y'].max()
+    mean_abs_y = rotated_df['acc_y'].abs().mean()
+    # ------------------------------------
+    mean_acc_z = rotated_df['acc_z'].mean()
+    median_acc_z = rotated_df['acc_z'].median()
+    std_acc_z = rotated_df['acc_z'].std()
+    min_acc_z = rotated_df['acc_z'].min()
+    max_acc_z = rotated_df['acc_z'].max()
+    mean_abs_z = rotated_df['acc_z'].abs().mean()
+
+    # accelerometer (including gravity) of this window frame
+    mean_acc_gx = rotated_df['acc_gx'].mean()
+    # ------------------------------------
+    mean_acc_gy = rotated_df['acc_gy'].mean()
+    # ------------------------------------
+    mean_acc_gz = rotated_df['acc_gz'].mean()
+
+    # other features
+    mean_magnitude = rotated_df['mean_magnitude'].mean()
+    # ------------------------------------
+    signal_magnitude_area = np.sum(np.abs([mean_acc_x, mean_acc_y, mean_acc_z]))
+    # ------------------------------------
+    mean_freq_x, dominant_freq_x = compute_frequency_domain(rotated_df['acc_x'], data_interval)
+    mean_freq_y, dominant_freq_y = compute_frequency_domain(rotated_df['acc_y'], data_interval)
+    mean_freq_z, dominant_freq_z = compute_frequency_domain(rotated_df['acc_z'], data_interval)
+    # ------------------------------------
+    lats = list(data['gps_lat'])
+    lons = list(data['gps_lon'])
+    diff_lat = lats[0] - lats[len(lats) - 1]
+    diff_lon = lons[0] - lons[len(lons) - 1]
+
+    row = pd.DataFrame([[mean_acc_x, median_acc_x, std_acc_x, min_acc_x, max_acc_x, mean_abs_x,
+                         mean_acc_y, median_acc_y, std_acc_y, min_acc_y, max_acc_y, mean_abs_y,
+                         mean_acc_z, median_acc_z, std_acc_z, min_acc_z, max_acc_z, mean_abs_z,
+                         mean_acc_gx, mean_acc_gy, mean_acc_gz,
+                         mean_magnitude, signal_magnitude_area,
+                         mean_freq_x, dominant_freq_x,
+                         mean_freq_y, dominant_freq_y,
+                         mean_freq_z, dominant_freq_z,
+                         diff_lat, diff_lon]], columns=features)
+    result_df = pd.concat([result_df, row], ignore_index=True)
+
+    return result_df
